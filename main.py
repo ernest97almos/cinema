53b87b9f6824bbae477aa91789885505ae4bee06
ernest <ernest@MacBookAir.lan>
2025-10-20 21:59:42 +0500

    Дал доступ фронту

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 from database import engine, Base
 from routers import router
 from movies import init_movies
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "https://kino-app-lbaz.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app = FastAPI(title="Cinema Booking API")
 
