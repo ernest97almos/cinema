47724f498a57012e6e4be8bb92892fafdad63ec7
ernest <ernest@MacBookAir.lan>
2025-10-26 01:13:28 +0500

    добавил еще ссылку в CORS

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Cinema Booking API")
 
 origins = [
+    "https://cinema-front-one.vercel.app/",
     "https://kino-app-lbaz.vercel.app/",
     "http://localhost:3000"  # обратите внимание на http://
 ]
